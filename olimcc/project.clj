245ca48a79da0969bfb70869f1c668b23589b5d8
Oli Mcc <dev@olimcc.com>
2012-06-03 22:15:16 -0700

    update lein-ring version

--- olimcc/project.clj
+++ olimcc/project.clj
@@ -4,6 +4,6 @@
                  [compojure "1.1.0"]
                  [ring/ring-core "1.1.0"]
                  [ring/ring-jetty-adapter "1.1.0"]]
-  :dev-dependencies [[lein-ring "0.5.0"]]
+  :dev-dependencies [[lein-ring "0.7.1"]]
   :ring {:handler olimcc.core/app})
 
