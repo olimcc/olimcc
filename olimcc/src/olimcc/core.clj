419013ff8e4acf40f74fc63910db096580b3750c
Oli Mcc <dev@olimcc.com>
2012-06-10 00:52:52 -0700

    serve up static file for index

--- olimcc/src/olimcc/core.clj
+++ olimcc/src/olimcc/core.clj
@@ -1,12 +1,14 @@
 (ns olimcc.core
-  (:use compojure.core)
-  (:require [compojure.route :as route]
+  (:use [ring.util.response]
+        [compojure.core])
+  (:require [clojure.java.io :as io]
+            [compojure.route :as route]
             [compojure.handler :as handler]))
 
 (defn handler []
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+  ;; doesn't seem to work:
+  ;; (file-response "test.html" {:root "pages"})
+  (resource-response "index.html" {:root "pages"}))
 
 (defroutes main-routes
   (GET "/" [] (handler))
