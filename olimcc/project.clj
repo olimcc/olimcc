350045cb9f971461066a8cbdb0c5ff8e36a78629
Oli Mcc <dev@olimcc.com>
2012-11-19 01:27:55 -0800

    increment project version to 0.6

--- olimcc/project.clj
+++ olimcc/project.clj
@@ -1,4 +1,4 @@
-(defproject olimcc "0.5"
+(defproject olimcc "0.6"
   :description "Olimcc site"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
