2205e660bd6ef5fce1538eac2aee0b7c44314a2a
Oli Mcc <oli.mcc@gmail.com>
2015-11-30 16:41:30 -0800

    Add updated location visual, contact info etc

--- olimcc/resources/public/static/js/main2.js
+++ olimcc/resources/public/static/js/main2.js
@@ -1,10 +1,39 @@
+var getPointsOnPath = function(pathNode, segments) {
+  var l = pathNode.getTotalLength(),
+    segmentLength = l/segments;
+  // segments + 1 to start at the 0th point, and finish at the last
+  return _.map(_.range(segments + 1), function(_, i) {
+    var distance = segmentLength * i,
+      point = pathNode.getPointAtLength(distance);
+    return {value: [point.x, point.y]};
+  });
+};
+
+var rdm = function(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+var getMail = function() {
+  var t = ['o','l','hi','i','@','@','@', 'o','l','i','m','c',
+           'yo','c','.','c','f','o','m'];
+  var res = t.slice(0, 2).concat(t.slice(3,5))
+                         .concat(t.slice(7,12))
+                         .concat(t.slice(13,16))
+                         .concat(t.slice(17,19));
+  return res.join('');
+};
+
 var initCharts = function(data) {
 
   var data = _.map(data.reverse(), function(d) {return {value: [d.lng, d.lat]}});
 
+  // general space setup
+  var rect = d3.select('body').node().getBoundingClientRect();
+  var header = d3.select('#header').node().getBoundingClientRect();
+
   var margin = {top: 60, right: 60, bottom: 90, left: 60};
-    width = 1440 - margin.right - margin.left,
-    height = 650 - margin.top - margin.bottom;
+    width = rect.width - margin.right - margin.left,
+    height = rect.height - header.height - margin.top - margin.bottom;
 
   var svg = d3.select('.chart')
       .attr('width', width + margin.left + margin.right)
@@ -12,28 +41,73 @@ var initCharts = function(data) {
     .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    var y = d3.scale.linear()
-      .range([height, 0])
-      .domain([d3.min(data, function(d) { return d.value[1]; }),
-               d3.max(data, function(d) { return d.value[1]; })]);
+  // setup line fn to translate geo coords
+  var y = d3.scale.linear()
+    .range([height, 0])
+    .domain([d3.min(data, function(d) { return d.value[1]; }),
+             d3.max(data, function(d) { return d.value[1]; })]);
 
-    var x = d3.scale.linear()
-      .range([0, width])
-      .domain([d3.min(data, function(d) { return d.value[0]; }),
-               d3.max(data, function(d) { return d.value[0]; })]);
+  var x = d3.scale.linear()
+    .range([0, width])
+    .domain([d3.min(data, function(d) { return d.value[0]; }),
+             d3.max(data, function(d) { return d.value[0]; })]);
 
-  // generate a line fn
   var line = d3.svg.line()
     .x(function(data) {return x(data.value[0])})
     .y(function(data) {return y(data.value[1])})
     .interpolate('cardinal');
 
-  svg.append('path')
-    .attr('d', line(data))
-    .style('stroke', 'silver')
-    .style('stroke-width', '1')
-    .style('fill', 'none')
+  var path = svg.append('path')
+              .attr('d', line(data));
 
+  // generate slightly augmented shadow lines
+  var segments = 50,
+      points = getPointsOnPath(path.node(), segments),
+      pixVariability = 12,
+      lines = 5,
+      fuzzyLine = d3.svg.line()
+            .x(function(data, i) {
+              return (i == 0 || i == segments) ?
+                                data.value[0] :
+                                data.value[0] + rdm(-pixVariability, pixVariability)})
+            .y(function(data, i) {
+              return (i == 0 || i == segments) ?
+                                data.value[1] :
+                                data.value[1] + rdm(-pixVariability, pixVariability)})
+            .interpolate('cardinal');
+
+  _.each(_.range(lines), function() {
+    svg.append('path')
+      .attr('d', fuzzyLine(points))
+      .style('stroke', 'silver')
+      .style('stroke-width', 4)
+      .style('stroke-opacity', rdm(0.05,0.15))
+      .style('fill', 'none');
+  });
+
+  // generate actual route line
+  var lineSegmentsLine = d3.svg.line()
+    .x(function(data) {return data[0]})
+    .y(function(data) {return data[1]});
+
+  var segments = 3000,
+    points = getPointsOnPath(path.node(), segments),
+    outX = _.compact(_.map(points, function(elem, index) {
+    if (index+1 != points.length) {
+      return [points[index].value, points[index+1].value];
+    }}));
+
+  svg.selectAll('path.mainRoute')
+    .data(outX)
+    .enter()
+    .append('path')
+    .attr('d', function(d) {return lineSegmentsLine(d)})
+    .style('stroke', 'silver')
+    .style('stroke-width', 2)
+    .style('stroke-opacity', function(d, i) {return i/segments})
+    .style('fill', 'none');
+
+  // generate waypoint circles
   svg.selectAll('circle.locations')
     .data(data)
     .enter()
@@ -43,169 +117,74 @@ var initCharts = function(data) {
     .attr('r', 4)
     .style('fill', 'silver')
     .style('stroke', 'black')
-    .style('stroke-width', '3')
+    .style('stroke-width', 2)
     .on('click', function(d) {
-      console.log(d.value.reverse())
-    })
+      console.log(d.value.reverse());
+    });
 
-    // headphones
-    /*
-    var centre = [x(data[data.length-1].value[0]), y(data[data.length-1].value[1])]
-    var pi = Math.PI;
-    var arc = d3.svg.arc()
-        .innerRadius(108)
-        .outerRadius(110)
-        .startAngle(-60 * (pi/180)) //converting from degs to radians
-        .endAngle(140 * (pi/180)) //just radians
-    svg.append('path')
-      .attr('class', 'headphones')
-      .attr('d', arc)
-      .attr('fill', 'gray')
-      .attr('transform', 'translate(' + x(data[data.length-1].value[0]) + ',' + y(data[data.length-1].value[1]) + ')');
-
-    svg.append('rect')
-      .attr('class', 'headphones')
-      .attr('x', 498)
-      .attr('y', 216)
-      .attr('width', 15)
-      .attr('height', 40)
-      .attr('transform', 'rotate(38, 500, 218)')
-
-    svg.append('rect')
-      .attr('class', 'headphones')
-      .attr('x', 670)
-      .attr('y', 355)
-      .attr('width', 15)
-      .attr('height', 40)
-      .attr('transform', 'rotate(38, 671, 356)')
-
-      // headphones text
-
-      var pi = Math.PI;
-      var arc = d3.svg.arc()
-          .innerRadius(110)
-          .outerRadius(117)
-          .startAngle(-60 * (pi/180)) //converting from degs to radians
-          .endAngle(140 * (pi/180)) //just radians
-      svg.append('path')
-        .attr('id', 'headphone-text')
-        .attr('d', arc)
-        .attr('fill', 'none')
-        .attr('transform', 'translate(' + x(data[data.length-1].value[0]) + ',' + y(data[data.length-1].value[1]) + ')');
-
-      svg.append('text')
-       .append('textPath') //append a textPath to the text element
-       .attr('id', 'headphone-text')
-    	 .attr('xlink:href', '#headphone-text') //place the ID of the path here
-    	 //.style('text-anchor','middle') //place the text halfway on the arc
-    	 .attr('startOffset', '6.5%')
-       .attr('transform', 'translate(100, 100)')
-    	 .text('Flying Lotus - Massage Situation');
-       */
-
-    // main circle
-    /*
-    var coreCircle = svg.append('circle')
+  // generate pulsing location circle
+  var makePulseCircle = function(data, times) {
+    // base circle
+    var pulseCircle = svg.append('circle')
       .attr('cx', x(data[data.length-1].value[0]))
       .attr('cy', y(data[data.length-1].value[1]))
-      .attr('r', 100)
+      .attr('r', 0)
       .style('fill', 'white')
-      .style('stroke', 'black')
-      .style('stroke-width', '2');
-      */
+      .style('fill-opacity', 0)
+      .style('stroke', 'gray')
+      .style('stroke-width', 2)
+      .style('stroke-opacity', .7);
 
-    var times = 5;
+    pulseCircle.transition()
+      .duration(1500)
+      .attr('r', 200)
+      .style('stroke-opacity', 0)
+      .each('end', function() {
+        var count = 1;
+        var pC = d3.select(this);
+        (function repeat() {
+          count+=1;
+          var radius = (count == times) ? 600 : 200,
+              duration = (count == times) ? 1900 : 1500;
+          pC.attr('r', 0)
+            .style('stroke-opacity', .7);
+          pC.transition()
+            .delay(600)
+            .duration(duration)
+            .attr('r', radius)
+            .style('stroke-opacity', 0)
+            .each('end', function() {
+              (count < times) ? repeat() : null;
+            });
+        })();
+      })
+  }
 
-    var makePulseCircle = function() {
-      // base circle
-      var pulseCircle = svg.append('circle')
-        .attr('cx', x(data[data.length-1].value[0]))
-        .attr('cy', y(data[data.length-1].value[1]))
-        .attr('r', 0)
-        .style('fill', 'white')
-        .style('fill-opacity', '0')
-        .style('stroke', 'gray')
-        .style('stroke-width', '2')
-        .style('stroke-opacity', .7);
-
-      pulseCircle.transition()
-        .duration(1500)
-        .attr('r', 200)
-        .style('stroke-opacity', 0)
-        .each('end', function() {
-          var count = 1;
-          var pC = d3.select(this);
-          (function repeat() {
-            count+=1;
-            pC.attr('r', 0)
-              .style('stroke-opacity', .7);
-            pC.transition()
-              .delay(600)
-              .duration(1500)
-              .attr('r', 200)
-              .style('stroke-opacity', 0)
-              .each('end', function() {
-                (count < times) ? repeat() : null;
-              });
-          })();
-        })
-    }
-
-    makePulseCircle();
-    window.setTimeout(function() {
-      makePulseCircle();
-    }, 140)
-
-
-
-      /*
-      var pulseCircle2 = svg.append('circle')
-        .attr('cx', x(data[data.length-1].value[0]))
-        .attr('cy', y(data[data.length-1].value[1]))
-        .attr('r', 0)
-       .style('fill', 'white')
-       .style('fill-opacity', '0')
-        .style('stroke', 'gray')
-        .style('stroke-width', '2')
-        .style('stroke-opacity', .2);
-
-      pulseCircle2.transition()
-        .duration(1200)
-        .delay(100)
-        .attr('r', 98)
-        .style('stroke', 'white')
-        .each('end', function() {
-          var pC = d3.select(this);
-          (function repeat() {
-            pC.attr('r', 0)
-              .style('stroke', 'gray');
-            pC.transition()
-              .delay(500)
-              .duration(1200)
-              .attr('r', 98)
-              .style('stroke', 'white')
-              .each('end', repeat);
-          })();
-        })
-        */
-
-
-
-
-  /*
-  svg.selectAll('text')
-    .data(data)
-    .enter()
-    .append('text')
-    .attr('x', function(d){return x(d.value[0]) + 6})
-    .attr('y', function(d){return y(d.value[1]) + 12})
-    .text(function(d) {return d.name})
-  */
+  var pulseTimes = 5;
+  makePulseCircle(data, pulseTimes);
+  _.delay(function() {
+    makePulseCircle(data, pulseTimes);
+  }, 140);
 };
+
+var el = document.getElementById('email');
+el.innerHTML = getMail();
 
 var url = purl(),
   start = url.param('start'),
-  dest = start ? "http://olimcc.com/location?start=" + start : "http://olimcc.com/location";
+  limit = url.param('limit'),
+  dest = start ? "http://olimcc.com/location?start=" + start + '&' : "http://olimcc.com/location?",
+  dest = limit ? dest + 'limit=' + limit : dest;
 d3.json(dest, function(error, json) {
   initCharts(json);
-})
+});
+
+// set up tooltips
+html5tooltips({
+  animateFunction: "fadein",
+  color: "black",
+  contentText: "The visual on this page indicates my activity and movement over the past 2 hours.",
+  delay: 0,
+  stickTo: "left",
+  targetSelector: "#info"
+});
