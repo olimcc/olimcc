f3e9d9445d293bb01a89e06b5165354902059292
Oli Mcc <oli.mcc@gmail.com>
2016-10-18 18:42:51 -0700

    conditional build on tag

--- deploy-travis.sh
+++ deploy-travis.sh
@@ -1,18 +1,25 @@
-pip install awscli
-PATH=$PATH:$HOME/.local/bin
 
 #TRAVIS_BRANCH='test-branch'
 #TRAVIS_COMMIT='test-commit'
 #AWS_ECS_REGISTRY='ecs-reg'
+#TRAVIS_TAG='cool-tag'
 
-cd olimcc
-#aws s3 cp $APP_CONFIG_FILE ./.app-config.clj
-IMAGE_NAME=olimcc-app
-DATE=`date +%Y-%m-%d.%H%M%S`
-IMAGE_TAG=$DATE-$TRAVIS_BRANCH.$TRAVIS_COMMIT
-eval $(aws ecr get-login --region us-east-1)
 
-echo $IMAGE_NAME:$IMAGE_TAG
-docker build --build-arg GIT_COMMIT=$TRAVIS_COMMIT -t $IMAGE_NAME:$IMAGE_TAG .
-docker tag $IMAGE_NAME:$IMAGE_TAG $AWS_ECS_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
-docker push $AWS_ECS_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
+if [ -z "$TRAVIS_TAG" ]; then
+    echo "will not build, no git tag"
+else
+    pip install awscli
+    PATH=$PATH:$HOME/.local/bin
+
+    cd olimcc
+    #aws s3 cp $APP_CONFIG_FILE ./.app-config.clj
+    IMAGE_NAME=olimcc-app
+    DATE=`date +%Y%m%d.%H%M%S`
+    IMAGE_TAG=$DATE-$TRAVIS_BRANCH.${TRAVIS_COMMIT:0:10}
+    eval $(aws ecr get-login --region us-east-1)
+
+    echo "building" $IMAGE_NAME:$IMAGE_TAG
+    docker build --build-arg GIT_COMMIT=$TRAVIS_COMMIT -t $IMAGE_NAME:$IMAGE_TAG .
+    docker tag $IMAGE_NAME:$IMAGE_TAG $AWS_ECS_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
+    docker push $AWS_ECS_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
+fi
