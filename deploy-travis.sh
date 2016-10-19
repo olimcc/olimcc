284b08d371b12ed6a7fa1987bfb90a46129051a7
Oli Mcc <oli.mcc@gmail.com>
2016-10-18 17:59:23 -0700

    update travis config

---
+++ deploy-travis.sh
@@ -0,0 +1,14 @@
+pip install awscli
+PATH=$PATH:$HOME/.local/bin
+
+cd olimcc
+aws s3 cp $APP_CONFIG_FILE ./.app-config.clj
+IMAGE_NAME=olimcc-app
+DATE=`date +%Y-%m-%d.%H%M%S`
+IMAGE_TAG=$DATE-$TRAVIS_BRANCH.$TRAVIS_COMMIT
+eval $(aws ecr get-login --region us-east-1)
+
+echo $IMAGE_NAME:$IMAGE_TAG
+docker build --build-arg GIT_COMMIT=$TRAVIS_COMMIT -t $IMAGE_NAME:$IMAGE_TAG .
+docker tag $IMAGE_NAME:$IMAGE_TAG $AWS_ECS_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
+docker push $AWS_ECS_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
