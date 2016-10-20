560c46fd4b8009841a4035661f8c52307f5c7e20
Oli Mcc <oli.mcc@gmail.com>
2016-10-19 17:14:29 -0700

    use straight git cmds for build params

--- deploy-travis.sh
+++ deploy-travis.sh
@@ -3,7 +3,8 @@
 #TRAVIS_COMMIT='test-commit'
 #AWS_ECS_REGISTRY='ecs-reg'
 #TRAVIS_TAG='cool-tag'
-
+GIT_BRANCH=$(git symbolic-ref --short -q HEAD)
+GIT_HEAD=$(git rev-parse --short HEAD)
 
 if [ -z "$TRAVIS_TAG" ]; then
     echo "will not build, no git tag"
@@ -15,11 +16,11 @@ else
     #aws s3 cp $APP_CONFIG_FILE ./.app-config.clj
     IMAGE_NAME=olimcc-app
     DATE=`date +%Y%m%d.%H%M%S`
-    IMAGE_TAG=$DATE-$TRAVIS_BRANCH.${TRAVIS_COMMIT:0:10}
+    IMAGE_TAG=$DATE-$GIT_BRANCH.$GIT_HEAD
     eval $(aws ecr get-login --region us-east-1)
 
     echo "building" $IMAGE_NAME:$IMAGE_TAG
-    docker build --build-arg GIT_COMMIT=$TRAVIS_COMMIT -t $IMAGE_NAME:$IMAGE_TAG .
+    docker build --build-arg GIT_COMMIT=$GIT_HEAD -t $IMAGE_NAME:$IMAGE_TAG .
     docker tag $IMAGE_NAME:$IMAGE_TAG $AWS_ECS_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
     docker push $AWS_ECS_REGISTRY/$IMAGE_NAME:$IMAGE_TAG
 fi
